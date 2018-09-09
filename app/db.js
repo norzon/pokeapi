6fe26fd4200dbb2f89ba88657538bab6a426c45e
Alexandros Leontakianakos <alleon20@gmail.com>
2018-09-09 20:08:48 +0300

    Implement custom prepared statements

--- app/db.js
+++ app/db.js
@@ -13,6 +13,13 @@ class DB {
             database: settings.dtbs,
             port: settings.port
         });
+
+        // Save settings
+        this.settings = settings;
+    }
+
+    static initiate (settings) {
+        return new this(settings);
     }
 
     // Check db connection
@@ -30,17 +37,62 @@ class DB {
     }
 
     // Generic query wrapper
-    query (str) {
+    query (str, params = []) {
         const self = this;
         return new Promise(function(res, rej){
-            self.pool.query(str, function(error, results, fields){
-                if (error) {
-                    rej(error);
-                } else {
-                    res(results, fields);
-                }
-            });
+            if (params) {
+                self.pool.query(str, params, function(error, results, fields){
+                    if (error) {
+                        rej(error);
+                    } else {
+                        res(results, fields);
+                    }
+                });
+            } else {
+                self.pool.query(str, function(error, results, fields){
+                    if (error) {
+                        rej(error);
+                    } else {
+                        res(results, fields);
+                    }
+                });
+            }
         });
+    }
+
+    prepare (str) {
+        return new STATEMENT(str, this);
+    }
+}
+
+class STATEMENT {
+
+    constructor (str, db) {
+        // Check input
+        if (typeof str !== "string")
+            throw new TypeError('Query should be type: string')
+        
+        // Save the query string 
+        this._str = str;
+
+        // Save the database reference
+        this.db = db;
+    }
+
+    execute (input) {
+        let query = this._str;
+        const params = [];
+
+        if (input instanceof Array) {
+            params = input;
+        } else if (typeof input === "object") {
+            Object.keys(input).forEach(key => {
+                // query = query.replace(key, mysql.escape(input[key]));
+                query = query.replace(key, '?')
+                params.push(input[key])
+            });
+        }
+        return this.db.query(query, params);
     }
 }
 
